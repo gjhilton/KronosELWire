5e716874be558eb993503338dbbc902c20b3154c
gjhilton <gjh@robocoppelia.com>
2012-12-29 15:26:26 +0000

    all channels in

--- fauxneon/fauxneon.ino
+++ fauxneon/fauxneon.ino
@@ -2,19 +2,33 @@
 
 NeonChannel greenarrow("Green Arrow",2);
 NeonChannel karaoke("Karaoke",3);
+NeonChannel martini("Martini",4);
+NeonChannel cocktail("Cocktail",5);
+NeonChannel yellowarrow("Fly in arrow",6);
+NeonChannel flyin("Fly in ",7);
 
 void setup() {                
 	Serial.begin(9600);
 	
-	//karaoke.setToggleDelay(5000,1000);	
+	karaoke.setToggleDelay(5000,1000);
+	martini.setToggleDelay(2000,3000);
+	yellowarrow.setToggleDelay(1000,3000);	
 	//greenarrow.crackleMode();
 	
-	//karaoke.begin();
+	karaoke.begin();
 	greenarrow.begin();
+	martini.begin();
+	cocktail.begin();
+	yellowarrow.begin();
+	flyin.begin();
 }
 
 void loop() {
-	//karaoke.loop();
+	karaoke.loop();
 	greenarrow.loop();
+	martini.loop();
+	cocktail.loop();
+	yellowarrow.loop();
+	flyin.loop();
 	delay(50);
 }
